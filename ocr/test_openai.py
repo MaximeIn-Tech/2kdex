b888cd7e429ba9388c111025929f875372179c62
MaximeIn-Tech <pro.maximehamou@gmail.com>
2025-08-26 18:56:18 +0200

    Fonctionne pas trop mal sur la détection des zones

--- ocr/test_openai.py
+++ ocr/test_openai.py
@@ -9,10 +9,14 @@ from openai import OpenAI
 # CONFIG
 # ===========================
 <<<<<<< HEAD
+<<<<<<< HEAD
 client = OpenAI()
 =======
 client = OpenAI(api_key="key")
 >>>>>>> 721c0ca (Fonctionne pas trop mal sur la détection des zones et des noms.)
+=======
+client = OpenAI(api_key="API_KEY")
+>>>>>>> b70698a (Fonctionne pas trop mal sur la détection des zones et des noms.)
 # Dossiers
 OUTPUT_JSON_DIR = "../data/output/matches_json"
 OUTPUT_CSV_DIR = "../data/output/matches_csv"
